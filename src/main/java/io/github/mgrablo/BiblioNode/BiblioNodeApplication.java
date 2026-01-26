b1f1981d700907aa2e00e001e6e56d26bd1718c6
mgrablo <51509107+Michauu035@users.noreply.github.com>
2026-01-26 14:10:08 +0100

    init

---
+++ src/main/java/io/github/mgrablo/BiblioNode/BiblioNodeApplication.java
@@ -0,0 +1,13 @@
+package io.github.mgrablo.BiblioNode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mgrablo/BiblioNode/BiblioNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mgrablo.BiblioNode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
