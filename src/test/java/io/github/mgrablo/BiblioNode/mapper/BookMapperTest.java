38b07ff51aa4c3c72c48a6c89c4eb1d610c4fcb0
mgrablo <51509107+Michauu035@users.noreply.github.com>
2026-01-29 19:39:06 +0100

    test: add unit tests for BookMapper to validate mapping functionality

---
+++ src/test/java/io/github/mgrablo/BiblioNode/mapper/BookMapperTest.java
@@ -0,0 +1,25 @@
+package io.github.mgrablo.BiblioNode.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import io.github.mgrablo.BiblioNode.dto.BookResponse;
+import io.github.mgrablo.BiblioNode.model.Author;
+import io.github.mgrablo.BiblioNode.model.Book;
+
+public class BookMapperTest {
+	private final BookMapper mapper = Mappers.getMapper(BookMapper.class);
+
+	@Test
+	void shouldMapBookToBookResponse() {
+		Author author = new Author(10L, "AAA", "Bio", null);
+		Book book = new Book(1L, "BBB", "111", author);
+
+		BookResponse response = mapper.toResponse(book);
+
+		assertEquals(10L, response.authorId());
+		assertEquals("AAA", response.authorName());
+	}
+}
