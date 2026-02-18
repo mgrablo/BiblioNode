e7dd9cdb6a79f7dae91803b6808c5a0b1eb6fc2d
mgrablo <51509107+Michauu035@users.noreply.github.com>
2026-02-18 20:51:08 +0100

    feat: add Role entity and RoleName enum with database changelog for roles table

---
+++ src/main/java/io/github/mgrablo/BiblioNode/model/Role.java
@@ -0,0 +1,21 @@
+package io.github.mgrablo.BiblioNode.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20, unique = true, nullable = false)
+	private RoleName name;
+}
