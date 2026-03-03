faa38dd354464c6e56566f54f58da114ca7735ac
mgrablo <51509107+Michauu035@users.noreply.github.com>
2026-03-03 19:25:50 +0100

    refactor: update LoanProperties to use record type and simplify LoanServiceImpl

--- src/main/java/io/github/mgrablo/BiblioNode/BiblioNodeApplication.java
+++ src/main/java/io/github/mgrablo/BiblioNode/BiblioNodeApplication.java
@@ -2,8 +2,12 @@ package io.github.mgrablo.BiblioNode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import io.github.mgrablo.BiblioNode.config.LoanProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LoanProperties.class)
 public class BiblioNodeApplication {
 
 	public static void main(String[] args) {
